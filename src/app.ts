9ae569b7c38e4eaf4100ee7b6c582fa1319beb2c
hsn8najafi <hsn8najafi@gmail.com>
2022-01-21 10:42:27 +0330

    Refactoring

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,10 @@ import "dotenv/config";
 import express, { Application } from "express";
 import { Server } from "socket.io";
 
-// App
 const app: Application = express();
-
-// Main Server
 const server = http.createServer(app);
-
-// Socket.IO
 const io = new Server(server);
 
-// Set '/public' For Static Files
 app.use(express.static(path.join(__dirname, "public")));
 
 const { PORT, HOST, NODE_ENV } = process.env;
