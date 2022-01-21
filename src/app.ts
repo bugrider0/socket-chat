76d30bd2fdb4af9d816d35b773f43f3c0b0d2d5d
hsn8najafi <hsn8najafi@gmail.com>
2022-01-21 09:42:49 +0330

    socket.io

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import path from "path";
+import http from "http";
 
 import "dotenv/config";
 import express, { Application } from "express";
+import { Server } from "socket.io";
 
 // App
 const app: Application = express();
+
+// Main Server
+const server = http.createServer(app);
+
+// Socket.IO
+const io = new Server(server);
 
 // Set '/public' For Static Files
 app.use(express.static(path.join(__dirname, "public")));
 
 const { PORT, HOST, NODE_ENV } = process.env;
-app.listen(PORT, () =>
+server.listen(PORT, () =>
   console.log(
     `Server is Running on -> http://${HOST}:${PORT} and ${NODE_ENV} Mode`
   )
